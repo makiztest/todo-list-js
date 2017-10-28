b8f8beaa85d5d46aa654a4c7d96fd9d36d61ee6a
Mark Delos Reyes <makizworks@gmail.com>
2017-10-28 19:37:21 -0400

    add click event .delete

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,14 @@
 var li = document.querySelectorAll("li");
+var thrash = document.querySelectorAll(".delete")
 
-for(var i = 0; i < li.length; i++) {
-    li[i].addEventListener("click", function() {
-            this.classList.toggle("strikeThrough")
-    })
+for (var i = 0; i < li.length; i++) {
+  li[i].addEventListener("click", function() {
+    this.classList.toggle("strikeThrough");
+  });
+}
+
+for (var i = 0; i < thrash.length; i++) {
+  thrash[i].addEventListener("click", function() {
+    console.log("xxxx")
+  });
 }
