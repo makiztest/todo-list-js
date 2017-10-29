8898abc68ac246b19cc6b1a3ada0d291a2283685
Mark Delos Reyes <makizworks@gmail.com>
2017-10-29 00:30:02 -0400

    strike through and delete list item

--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,26 @@
-var li = document.querySelectorAll("li");
-var thrash = document.querySelectorAll(".delete");
 var input = document.querySelector("input[type='text']");
-var ul = document.querySelector("ul")
+var ul = document.querySelector("ul");
 
-for (var i = 0; i < li.length; i++) {
-  li[i].addEventListener("click", function() {
-    this.classList.toggle("strikeThrough");
-  });
-}
+ul.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('strikeThrough');
+  }
 
-for (var i = 0; i < thrash.length; i++) {
-  thrash[i].addEventListener("click", function() {
-    this.parentNode.remove();
-  });
-}
+  if (ev.target.tagName === 'SPAN') {
+    ev.target.parentNode.remove();
+  }
+}, false);
 
 input.addEventListener("keypress", function(e) {
-  if(e.which === 13) {
-    var todotext = this.value
-    var newLi = document.createElement("li")
-    var newSpan = document.createElement("span")
-    ul.appendChild(newLi);
-    newLi.appendChild(newSpan).setAttribute("class", "delete");
+  if (e.which === 13) {
+    var newLi = document.createElement("li");
+    var newSpan = document.createElement("span");
+    ul
+      .appendChild(newLi)
+      .appendChild(newSpan)
+      .setAttribute("class", "delete");
     newSpan.append("X ");
-    newLi.append(todotext);
+    newLi.append(this.value);
+    this.value = "";
   }
-})
+});
