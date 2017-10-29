7209b2f08826a7d3f3f16422d0f69186eba86f05
Mark Delos Reyes <makizworks@gmail.com>
2017-10-28 22:29:15 -0400

    keypress on input

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 var li = document.querySelectorAll("li");
-var thrash = document.querySelectorAll(".delete")
+var thrash = document.querySelectorAll(".delete");
+var input = document.querySelector("input[type='text']");
 
 for (var i = 0; i < li.length; i++) {
   li[i].addEventListener("click", function() {
@@ -9,6 +10,12 @@ for (var i = 0; i < li.length; i++) {
 
 for (var i = 0; i < thrash.length; i++) {
   thrash[i].addEventListener("click", function() {
-    this.parentNode.remove().fadeOut(200);
+    this.parentNode.remove();
   });
 }
+
+input.addEventListener("keypress", function(e) {
+  if(e.which === 13) {
+    console.log("enter")
+  }
+})
